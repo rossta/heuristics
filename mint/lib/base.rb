937b57dfe94e8f83a2061418e42e5c5c16690715
Ross Kaffenberger <rosskaff@gmail.com>
2010-09-23 09:52:57 -0400

    Subclass ExactChange

--- mint/lib/base.rb
+++ mint/lib/base.rb
@@ -1,7 +1,7 @@
 module Mint
   class Base
 
-    attr_reader :time
+    attr_reader :results, :coin_set
 
     def initialize(multiplier)
       @multiplier = multiplier.to_i
@@ -9,128 +9,23 @@ module Mint
       @results    = 1000000
     end
 
-    def run!
-      @results  = exact_change_number(*@coin_set)
-      run_incremental_division_in_range_strategy
-    end
+  end
 
-    def exact_change_number(*coin_set)
-      m = min_change_matrix = Array.new(100, 0)
-      total_exact_change_number = 0
+  class ExactChange < Base
+
+    def calculate_coin_set(*coin_set)
+      cost    = Array.new(100, 0)
+      results = 0
       (1..99).each do |i|
-        m[i] = coin_set.map { |coin| m[i - coin] + 1 if coin <= i }.compact.min
-        cost = i % 5 == 0 ? m[i] * @multiplier : m[i]
-        total_exact_change_number = total_exact_change_number + cost
-        break if total_exact_change_number > @results
+        cost[i]       = coin_set.map { |coin| cost[i - coin] + 1 if coin <= i }.compact.min
+        current_cost  = i % 5 == 0 ? cost[i] * @multiplier : cost[i]
+        results       = results + current_cost
+        break if results > @results
       end
-      total_exact_change_number
+      results
     end
 
-    def total_exact_change_number
-      @results
-    end
-
-    def coin_set_to_s(coin_set = @coin_set)
-      coin_set.join(", ")
-    end
-
-    def run_range_strategy
-      h = 1
-      dollar = 100
-      half_dollar = dollar / 2
-      i = h + 1
-      j = h + 2
-      k = h + 3
-      l = h + 4
-      while i <= (dollar / 8) - h
-        while j <= (dollar / 4) - i
-          while k <= (dollar / 2) - j
-            while l < dollar - k
-              results = exact_change_number(1, i, j, k, l)
-              # puts coin_set_to_s([1, i, j, k, l])
-              if @results > results
-                @results = results
-                @coin_set = [1, i, j, k, l]
-                # puts "New coin set #{coin_set_to_s}"
-              end
-              l += 1
-            end
-            k += 1
-            l = k + 1
-          end
-          j += 1
-          k = j + 1
-        end
-        i += 1
-        j = i + 1
-      end
-    end
-
-    def run_halving_strategy
-      h = 1
-      dollar = 100
-      half_dollar = dollar / 2
-      i = h + 1
-      j = h + 2
-      k = h + 3
-      l = h + 4
-      while i <= (dollar / 8) - h
-        while j <= (dollar / 4) - i
-          while k <= (dollar / 2) - j
-            while l < dollar - k
-              results = exact_change_number(1, i, j, k, l)
-              # puts coin_set_to_s([1, i, j, k, l])
-              if @results > results
-                @results = results
-                @coin_set = [1, i, j, k, l]
-                # puts "New coin set #{coin_set_to_s}"
-              end
-              l += 1
-            end
-            k += 1
-            l = k + 1
-          end
-          j += 1
-          k = j + 1
-        end
-        i += 1
-        j = i + 1
-      end
-    end
-
-    def run_incremental_division_strategy
-      h = 1
-      dollar = 100
-      half_dollar = dollar / 2
-      i = h + 1
-      j = h + 2
-      k = h + 3
-      l = h + 4
-      while i <= (dollar / 5)
-        while j <= (dollar / 4)
-          while k <= (dollar / 3)
-            while l <= (dollar / 2)
-              results = exact_change_number(1, i, j, k, l)
-              # puts coin_set_to_s([1, i, j, k, l])
-              if @results > results
-                @results = results
-                @coin_set = [1, i, j, k, l]
-                # puts "New coin set #{coin_set_to_s}"
-              end
-              l += 1
-            end
-            k += 1
-            l = k + 1
-          end
-          j += 1
-          k = j + 1
-        end
-        i += 1
-        j = i + 1
-      end
-    end
-
-    def run_incremental_division_in_range_strategy
+    def calculate!
       h = 1
       dollar = 100
       l_ceil  = dollar / 2
@@ -148,7 +43,7 @@ module Mint
         while j <= j_ceil
           while k <= k_ceil
             while l <= l_ceil
-              results = exact_change_number(h, i, j, k, l)
+              results = calculate_coin_set(h, i, j, k, l)
               # puts coin_set_to_s([1, i, j, k, l])
               if @results > results
                 @results = results
@@ -167,5 +62,6 @@ module Mint
         j = [i + 1, j_floor].max
       end
     end
+
   end
 end
--- mint/lib/mint.rb
+++ mint/lib/mint.rb
@@ -1,10 +1,10 @@
 t_1     = Time.now
 require 'lib/base'
 raise "Usage: mint [integer]" if ARGV.length != 1
-mint    = Mint::Base.new(ARGV[0])
-mint.run!
-t_2     = Time.now
+mint    = Mint::ExactChange.new(ARGV[0])
 puts "Calculating...\n"
-puts "Total exact change number   : #{mint.total_exact_change_number}"
-puts "Coin set                    : #{mint.coin_set_to_s}"
+mint.calculate!
+t_2     = Time.now
+puts "Total exact change number   : #{mint.results}"
+puts "Coin set                    : #{mint.coin_set.join(", ")}"
 puts "Running time                : #{("%.3f" % (t_2 - t_1)).to_f}"
